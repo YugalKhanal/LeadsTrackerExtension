05bdfe1cdd468d5d8f8a18d2d3fa6c23f3870679
Yugal Khanal <yugalkhanal5@gmail.com>
2022-07-26 21:38:55 +0545

    Practiced .addEventListener

---
+++ index.js
@@ -0,0 +1,11 @@
+//first
+let inputEl = document.getElementById("input-btn");
+inputEl.addEventListener("click", function(){
+    console.log("Button clicked");
+})
+
+//second
+let boxEl = document.getElementById("box");
+boxEl.addEventListener("click", function() {
+    console.log("I want to open the box")
+})
