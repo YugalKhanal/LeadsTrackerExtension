1b3899678073eeb2b11ffd5da659a4febc2aa5df
Yugal Khanal <yugalkhanal5@gmail.com>
2022-07-27 14:25:39 +0545

    random

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-//first
-let inputEl = document.getElementById("input-btn");
-inputEl.addEventListener("click", function(){
-    console.log("Button clicked");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    // for (let i = 0; i < myLeads.length; i++) {
+    //     console.log(myLeads[i])
+        
+    // }
+    console.log(myLeads)
 })
 
-//second
-let boxEl = document.getElementById("box");
-boxEl.addEventListener("click", function() {
-    console.log("I want to open the box")
-})
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+
+const btnEl = document.querySelector(".button-el")
+btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy(){
+    btnEl.innerHTML += "<br>Thank you for buying!"
+}
