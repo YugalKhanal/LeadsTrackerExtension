39ea7fa8aa3e9246237ebdf1f1d61b6a7ad9a4b8
Yugal Khanal <yugalkhanal5@gmail.com>
2022-07-27 17:34:53 +0545

    Added innerHtml

--- index.js
+++ index.js
@@ -13,9 +13,19 @@ inputBtn.addEventListener("click", function(){
 })
 
 
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" 
+    //same thing as above
+    
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+    listItems += "<li>" + myLeads[i] + "</li>" 
 }
+ulEl.innerHTML = listItems
+
+
 
 const btnEl = document.querySelector(".button-el")
 btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
@@ -23,3 +33,4 @@ btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
 function buy(){
     btnEl.innerHTML += "<br>Thank you for buying!"
 }
+
