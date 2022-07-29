249d25687e9eeb966b830d61bcb53a5678ac16d1
Yugal Khanal <yugalkhanal5@gmail.com>
2022-07-29 19:49:13 +0545

    added json file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+// const btnEl = document.querySelector(".button-el")
+// btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// function buy(){
+//     btnEl.innerHTML += "<br>Thank you for buying!"
+// }
+
 let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
+
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
@@ -13,10 +22,9 @@ inputBtn.addEventListener("click", function(){
 function renderLeads () {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        /* const li = document.createElement("li")
+        li.textContent = myLeads[i]
+        ulEl.append(li)*/
         listItems += `
         <li>
             <a target='_blank' href='${myLeads[i]}'>
@@ -29,11 +37,12 @@ function renderLeads () {
 }
 
 
+const reciepient = "James"
+const sender = "Yugal"
 
-const btnEl = document.querySelector(".button-el")
-btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
-
-function buy(){
-    btnEl.innerHTML += "<br>Thank you for buying!"
-}
-
+const email = `
+Hey, ${reciepient}! 
+How is it going? 
+Cheers ${sender}
+`
+console.log(email)
