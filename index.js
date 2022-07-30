cd6997af8117c6599e55620c5d0de4efe40a246f
Yugal Khanal <yugalkhanal5@gmail.com>
2022-07-30 11:28:12 +0545

    added save and delete functionality

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-// const btnEl = document.querySelector(".button-el")
-// btnEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
-
-// function buy(){
-//     btnEl.innerHTML += "<br>Thank you for buying!"
-// }
-
-let myLeads = [];
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
+//getting the inputted value and turning to a string
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+//adding the inputted value to the array 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-    inputEl.value = ""
-    renderLeads()
+//on double click deletes all the inputted values
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 })
 
-function renderLeads () {
+//adds value to the array and localStorage after stringifying
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log(localStorage.getItem("myLeads"))
+})
+
+//lists on the page the values in the array
+function render (leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         /* const li = document.createElement("li")
         li.textContent = myLeads[i]
         ulEl.append(li)*/
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
             </a>
         </li>
         `
     }
     ulEl.innerHTML = listItems
-}
-
-
-const reciepient = "James"
-const sender = "Yugal"
-
-const email = `
-Hey, ${reciepient}! 
-How is it going? 
-Cheers ${sender}
-`
-console.log(email)
+} 
